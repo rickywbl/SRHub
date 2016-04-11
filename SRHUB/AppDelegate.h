f3cf79d85abbe8c171f61012e8912b69b44d7e62
SirRicky <525023395@qq.com>
2016-04-11 11:26:15 +0800

    LoadingView First Commit

---
+++ SRHUB/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SRHUB
+//
+//  Created by 王保霖 on 16/4/7.
+//  Copyright © 2016年 Ricky. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SRHUB/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SRHUB
+//
+//  Created by 王保霖 on 16/4/7.
+//  Copyright © 2016年 Ricky. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
